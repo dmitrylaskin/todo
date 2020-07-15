e26301659a77a9f18b8ce0fe396f96e0eb924209
Dmitry <boombapkilla@yandex.ru>
2020-07-15 13:16:41 +0300

    Updates

---
+++ precache-manifest.76538f8643ab9efb8896e26b753b970b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ba06c0ca37e703a3b0ed10b25ba9fc3",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "3521ddb992c602f9959d",
+    "url": "/todo/static/css/main.baa309f2.chunk.css"
+  },
+  {
+    "revision": "c1f5433762372afe4ebd",
+    "url": "/todo/static/js/2.44efbf2f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.44efbf2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3521ddb992c602f9959d",
+    "url": "/todo/static/js/main.7eae807f.chunk.js"
+  },
+  {
+    "revision": "01bbd3c1160f890aff3c",
+    "url": "/todo/static/js/runtime-main.3dc46a27.js"
+  }
+]);
